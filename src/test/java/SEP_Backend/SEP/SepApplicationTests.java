f536a56f419d8c153b18c94ce073b88e925de3ca
Additional Director GSWS <additionaldirectorgsws@192.168.1.3>
2024-06-09 08:52:08 +0530

    Developing Project

---
+++ src/test/java/SEP_Backend/SEP/SepApplicationTests.java
@@ -0,0 +1,13 @@
+package SEP_Backend.SEP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
